74b7447892877f1e7bf88c41f4262fe5d89cfa49
gumushobeangel-del <gumushobeangel@gmail.com>
2026-03-22 16:57:50 +0300

    my work

---
+++ script.py
@@ -0,0 +1,17 @@
+# Computer Programming is the art and science of writing computer instructions that is to say programming tells a computer what to do step by step.
+# Memory Partitioning is the process of splitting computer memory into small parts so that different programs can use it at the same time whereby it means dividing big room into smaller spaces so each task has its own place.
+# Memory Address tells where data is stored forexample  a notebook: you can write something in a notebook and when you want it you can just go back to your notebook and get the information back.
+# The CPU doesnot access values directly but it stores their addresses. The CPU deals with the addresses from RAM.The CPU is the part of a computer that thinks and makes decisions on a particular task.It reads instructions, understands them and tells other parts what to do.
+# The CPU is the brain of the computer that controls and processes everything.
+# The hard disk is a metallic ,circular and when it is new ,it is unusable until it undergoes through a process called partitioning and when it is in use ,it rotates clockwise.
+# The hard Disk is the part of a computer that stores information permanently forexample its like when you can keep your clothes in a wodrop and when you go anywhere your clothes can can stay there thats the same thing with the hard disk.
+# A variable is memory location in a computer that is used to store data for instance think of a labeled container ,container is the variable ,the label on the box is the variable name ,and the stuff inside is the data so thats the same a s variable
+# A statement is a single complete instruction to a computer forexample when they tell you add salt to sauce.
+# A code is a set of two or more statements forexample when your going to do something,you first plan and after do something thats the way as for a code whereby it can tell a computer what to do step by step.
+# A block of code is a set of two or more related statements
+# name = "Karungi"
+# age = "20"
+# hips = "27"
+# height = "5.7"
+# Declaring is the process of creating a variable
+# Initializing is the process of giving a value to a variable
