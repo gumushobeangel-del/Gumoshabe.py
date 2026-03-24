9c58b12e1e58e98c4c913145252367eef6f93510
gumushobeangel-del <gumushobeangel@gmail.com>
2026-03-24 15:36:59 +0300

    changes

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 # Computer Programming is the process of creating a set of instructions that tells a computer how to solve a problem that is to say programming tells a computer what to do step by step.
-# Memory Partitioning is when a computer splits its RAM into separate parts so that multiple programs can run safely into small parts so that different programs can use it at the same time whereby it means dividing big room into smaller spaces so each task has its own place.
+# Memory Partitioning is when a computer splits its RAM into separate parts so that multiple programs can run safely at the same time whereby it means dividing big room into smaller spaces so each task has its own place.
 # Memory Address is the location in memory where data is stored in forexample  a notebook: you can write something in a notebook and when you want it you can just go back to your notebook and get the information back.
 # The CPU doesnot access values directly but it stores their addresses. The CPU deals with the addresses from RAM.The CPU is the part of a computer that thinks and makes decisions on a particular task.It reads instructions, understands them and tells other parts what to do.
 # The CPU is the brain of the computer that controls and processes everything.
 # The CPU( Central Processing Unit) processes data and uses memory addresses to access the stored values in a computer and it usually called the brain of the computer because it processes instructions from programs and controls the parts of a computer
 # The CPU saves the work back to the memory incase it is needed
 # The hard disk is a metallic ,circular and when it is new ,it is unusable until it undergoes through a process called partitioning and when it is in use ,it rotates clockwise.
-# The hard Disk is the part of a computer that stores information permanently forexample its like when you can keep your clothes in a wodrop and when you go anywhere your clothes can can stay there thats the same thing with the hard disk.
+# The hard Disk is the part of a computer that stores information permanently forexample its like when you can keep your clothes in a wodrop and when you go anywhere your clothes can stay there thats the same thing with the hard disk.
 # A variable is a place in a memory where a program can store and change data while it runs for instance think of a labeled container ,container is the variable ,the label on the box is the variable name ,and the stuff inside is the data so thats the same as variable
 # A statement is a command that a computer carries out when the program runs forexample when they tell you add salt to sauce its like they have given you a command
 # A code is a set of instructions that control how a computer behaves forexample when your going to do something,you first plan and after do something thats the way as for a code whereby it can tell a computer what to do step by step.
